76932e2a58ae6b514edaa7371d9ca6722f7fdeb5
Hafidz Mahrus <havus.it@gmail.com>
2023-06-04 18:45:42 +0800

    submit

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,21 @@
+# sudo chmod 755 build_push_image_karsajobs_ui.sh
+
+# declare some variables
+image_name=ghcr.io/havus/karsajobsui
+image_version=latest
+image_name_with_version="$image_name:$image_version"
+
+# build image from current working dir
+docker build -t $image_name_with_version .
+
+# print all images
+docker images
+
+# create env var CR_PAT to store github package password
+# export CR_PAT=ghp_sometoken
+
+# Login to github container registry (ghcr)
+echo $CR_PAT | docker login ghcr.io --username havus --password-stdin
+
+# push image to container registry
+docker push $image_name_with_version
